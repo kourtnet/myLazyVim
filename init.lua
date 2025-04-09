0972769d176292ce4174bdd3614c55ec62ff1973
KourtnetPC <kourtnet@gmail.com>
2025-04-09 23:58:06 +0300

    got rid of terminal launching at start cuase it's pretty annoying

--- init.lua
+++ init.lua
@@ -11,16 +11,3 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 
 vim.g.python3_host_prog = "~/.venvs/neovim/bin/python"
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    require("toggleterm").toggle(1) -- Открытие терминала с ID 1
-  end,
-})
-
-require("toggleterm").setup({
-  direction = "horizontal",
-  size = 10, -- Установите желаемую высоту (например, 10 строк)
-})
-
-vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>", { desc = "Открыть/закрыть терминал" })
